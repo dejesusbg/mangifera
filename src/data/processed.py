0ad259120f07cd7e9622be17d93975146dbaed1e
ricardo barrios <110137626+dejesusbg@users.noreply.github.com>
2024-10-31 17:32:29 -0500

    refactor

--- src/data/processed.py
+++ src/data/processed.py
@@ -8,31 +8,37 @@ from src.data.image import MangoImage as Mango
 class MangoProcessor:
     CSV_DIR = "../data/processed"
 
-    def __init__(self, path, image_set=None):
-        self.image_set = image_set or []
-        self.mango = Mango
-        Mango.PATH = path
+    def __init__(self, image_set=[]):
+        self.image_set = image_set
 
         os.makedirs(self.CSV_DIR, exist_ok=True)
         self.csv_path = os.path.join(self.CSV_DIR, "data.csv")
         self.processed_data = CSVData(self.csv_path, data_generator=self._process)
 
     def _process(self):
-        return [dict(self.mango(image)) for image in self.image_set]
+        return [dict(Mango(image)) for image in self.image_set]
 
     def get_processed_data(self):
         return self.processed_data
 
     def load_original(self, use, image):
-        return self.mango.load_original(use, image)
+        return Mango.load_original(use, image)
 
-    def load_processed(self, image, size=(64, 64)):
-        img_values = list(image.values())[1:4097]
-        img_array = np.array(img_values).reshape(size)
+    def _extract_values(self, image):
+        values = list(image.values())
+        size = int((len(values) - (255 * 3) - 1) ** 0.5)
+
+        edges_bound = size * size + 1
+        edges, histogram = values[1:edges_bound], values[edges_bound:]
+        return edges, histogram, size
+
+    def load_processed(self, image):
+        img_values, _, size = self._extract_values(image)
+        img_array = np.array(img_values).reshape((size, size))
         return img_array
 
     def load_histogram(self, image):
-        hist = list(image.values())[4097:]
+        _, hist, _ = self._extract_values(image)
         bins = np.arange(256)
 
         b_hist = hist[0:256]
@@ -40,7 +46,6 @@ class MangoProcessor:
         r_hist = hist[512:768]
 
         plt.figure(figsize=(10, 5))
-
         plt.plot(bins, b_hist, color="blue", label="Blue Channel")
         plt.plot(bins, g_hist, color="green", label="Green Channel")
         plt.plot(bins, r_hist, color="red", label="Red Channel")
@@ -49,5 +54,4 @@ class MangoProcessor:
         plt.xlabel("Pixel Intensity")
         plt.ylabel("Frequency")
         plt.legend()
-
         plt.show()
