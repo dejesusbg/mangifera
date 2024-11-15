9801a1b96b646219c577a63d806e829dfd34545a
dejesusbg5 <dejesusbg5@gmail.com>
2024-11-14 22:00:33 -0500

    minor fix

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,12 +1,12 @@
 from .decision import MangoTree as decision_tree
-from .regression import MangoRegression as mv_regression
+from .regression import MangoRegression as multivariate_regression
 from .forest import MangoForest as random_forest
 from .deep import DeepMangoNetwork as deep_neural_network
 from .neural import MangoNetwork as neural_network
 
 __all__ = (
     "decision_tree",
-    "mv_regression",
+    "multivariate_regression",
     "random_forest",
     "deep_neural_network",
     "neural_network",
