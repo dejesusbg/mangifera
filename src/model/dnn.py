b8936bb1e43659debbf5e6f1fc24b220405912f3
dejesusbg5 <dejesusbg5@gmail.com>
2024-11-18 16:20:04 -0500

    increase layers to add depth

--- src/model/dnn.py
+++ src/model/dnn.py
@@ -1,31 +1,40 @@
 import matplotlib.pyplot as plt
 from tf_keras.models import Sequential
-from tf_keras.layers import Dense
+from tf_keras.layers import Dense, Dropout
 from . import mango
 
 
 class MangoDeepNetwork(mango):
-    def __init__(self, X, y, epochs=20, batch_size=32, class_weight=None):
+    def __init__(
+        self,
+        X,
+        y,
+        epochs=30,
+        batch_size=32,
+        layers=[256, 128, 64, 32, 16],
+        class_weight=None,
+    ):
         super().__init__(X, y)
+
+        self.model = Sequential()
+
+        self.model.add(
+            Dense(layers[0], input_dim=self.X_train.shape[1], activation="relu")
+        )
+
+        for layer_size in layers[1:]:
+            self.model.add(Dense(layer_size, activation="relu"))
+            self.model.add(Dropout(0.3))
+
+        self.model.add(Dense(1, activation="sigmoid"))
+
+        self.model.compile(
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
 
         self.epochs = epochs
         self.batch_size = batch_size
         self.class_weight = class_weight
-
-        layers = [
-            Dense(128, input_dim=self.X_train.shape[1], activation="relu"),
-            Dense(32, activation="relu"),
-            Dense(1, activation="sigmoid"),
-        ]
-
-        self.model = Sequential(layers)
-
-        self.model.compile(
-            loss="binary_crossentropy",
-            optimizer="adam",
-            metrics=["accuracy"],
-        )
-
         print("Model initialized successfully.")
 
     def train(self):
